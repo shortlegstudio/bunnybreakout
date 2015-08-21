f364473ac5e90a753289f3f25227fd07fd75dd98
Trevor Redfern <trevorredfern@gmail.com>
2015-08-20 22:24:24 -0700

    Scene layout and organization
    Updated the basic scenes so that it is possible to navigate between
    scenes. Started including some other fonts that will need to be cleaned
    out at some point as well.

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+	private static GameObject instance;
+
+
+	// Use this for initialization
+	void Awake () {
+		if (instance == null) {
+			instance = gameObject;
+			GameObject.DontDestroyOnLoad (gameObject);
+		} else {
+			GameObject.Destroy(gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
