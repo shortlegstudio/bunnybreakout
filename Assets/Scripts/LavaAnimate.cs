787176ff8011d60584da9ef777c26ccce151d820
Trevor Redfern <trevorredfern@gmail.com>
2015-10-17 14:03:09 -0700

    Added lava with simple animation

---
+++ Assets/Scripts/LavaAnimate.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LavaAnimate : MonoBehaviour {
+	public float _flowSpeed;
+	public float _minX = -0.5f;
+	public float _maxX = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = new Vector3 ();
+		pos.x = transform.position.x - _flowSpeed * Time.deltaTime;
+
+		//flip around if we have looped
+		if (pos.x < _minX)
+			pos.x = _maxX;
+
+		pos.y = transform.position.y;
+		pos.z = transform.position.z;
+
+		transform.position = pos;
+
+	}
+}
